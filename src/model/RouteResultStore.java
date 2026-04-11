7165be9065cc4b2ea518ba792d7e32124ff5a71c
Kardelen <kardelen.kartal@gmail.com>
2026-04-11 17:41:13 +0300

    Update RouteResultStore.java

--- src/model/RouteResultStore.java
+++ src/model/RouteResultStore.java
@@ -1,1 +1,24 @@
-{}
+package model;
+
+public class RouteResultStore {
+    private int routeId;
+    private String storeId;
+
+    public RouteResultStore() {}
+
+    public RouteResultStore(int routeId, String storeId) {
+        this.routeId = routeId;
+        this.storeId = storeId;
+    }
+
+    public int getRouteId() { return routeId; }
+    public void setRouteId(int routeId) { this.routeId = routeId; }
+
+    public String getStoreId() { return storeId; }
+    public void setStoreId(String storeId) { this.storeId = storeId; }
+
+    @Override
+    public String toString() {
+        return "RouteResultStore{routeId=" + routeId + ", storeId='" + storeId + "'}";
+    }
+}
