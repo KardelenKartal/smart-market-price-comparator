8deb8cd83431eaba2c2f0869517b8d118e061f9f
Kardelen <kardelen.kartal@gmail.com>
2026-04-11 17:38:34 +0300

    Update BasketItem.java

--- src/model/BasketItem.java
+++ src/model/BasketItem.java
@@ -1,6 +1,30 @@
 package model;
 
 public class BasketItem {
+    private int basketId;
+    private String stockItemId;
+    private int quantity;
 
-  
+    public BasketItem() {}
+
+    public BasketItem(int basketId, String stockItemId, int quantity) {
+        this.basketId = basketId;
+        this.stockItemId = stockItemId;
+        this.quantity = quantity;
+    }
+
+    public int getBasketId() { return basketId; }
+    public void setBasketId(int basketId) { this.basketId = basketId; }
+
+    public String getStockItemId() { return stockItemId; }
+    public void setStockItemId(String stockItemId) { this.stockItemId = stockItemId; }
+
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+    @Override
+    public String toString() {
+        return "BasketItem{basketId=" + basketId + ", stockItemId='" + stockItemId +
+               "', quantity=" + quantity + "}";
+    }
 }
